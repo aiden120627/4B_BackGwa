59c5d4e10b95a1747e5c2997592f2958baece352
aiden120627 <aiden24-12@naver.com>
2026-08-31 22:22:00 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Supabase 프로젝트를 만든 뒤 아래 두 값을 넣으세요.
+// Project Settings → API → Project URL / anon public key
+export const SUPABASE_URL = "https://fmmxwvvrontwpuafbcxi.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtbXh3dnZyb250d3B1YWZiY3hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxNjk3NzEsImV4cCI6MjEwMzc0NTc3MX0.y7CvYBeA-nKGX20SfMpxABRIW8ufqGvw4iNZDtMonbs";
